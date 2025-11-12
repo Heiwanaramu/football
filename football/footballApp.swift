1c459ceb190edd189b72a4240e0e6ef25cc55dae
Murali <muraliramdj@icloud.com>
2025-11-12 23:40:16 +0530

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Muraliraman on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
